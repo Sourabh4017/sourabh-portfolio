d5834c71dfdf5e7dcba6a35214aed02703f6ba57
Devmehra951 <0506cs233d16@gmail.com>
2025-12-15 11:05:38 -0600

    Initial commit - MERN portfolio project

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Sourbh Mehra </span>
+            from <span className="purple"> Bhopal, India.</span>
+            <br />
+            I am currently pursuing Batuler of Technology form Bansal institute of research technology and science, Bhopal.
+            <br />
+            I have completed Diploma from SV. Polytechnic College, Bhopal.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Dancing
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Miusic
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Sourabh</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
